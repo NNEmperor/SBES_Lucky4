0f0ce00b4b56cf80e1e51a6e0c6e2b964b7f098d
Nikola Nikolic <nikolic.nikola2312@gmail.com>
2020-12-24 13:09:48 +0100

    test2

--- RemoteServer/Program.cs
+++ RemoteServer/Program.cs
@@ -17,31 +17,61 @@ namespace RemoteServer
         {
             string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
 
-            //NetTcpBinding binding = new NetTcpBinding();
-            //binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            ServiceHost hostProcess = null;
+            ServiceHost hostServis = null;
 
-            //string address = "net.tcp://localhost:9998/Server";
-            //ServiceHost host = new ServiceHost(typeof(Server));
-            //host.AddServiceEndpoint(typeof(IServer), binding, address);
-
-            using (ServiceHost hostServis = new ServiceHost(typeof(Server)),
-                hostProcess = new ServiceHost(typeof(ProcessState)))
+            try
             {
+                NetTcpBinding binding = new NetTcpBinding();
+                binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
+                string address = "net.tcp://localhost:9901/Server";
+                hostServis = new ServiceHost(typeof(Server));
+                hostServis.AddServiceEndpoint(typeof(IServer), binding, address);
+
+                NetTcpBinding binding2 = new NetTcpBinding();
+                string address2 = "net.tcp://localhost:9911/Process";
+                hostProcess = new ServiceHost(typeof(ProcessState));
+                hostProcess.AddServiceEndpoint(typeof(IServerState), binding2, address2);
 
                 hostServis.Credentials.ClientCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.ChainTrust;
                 hostServis.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
                 hostServis.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
 
-                hostServis.Open();
-                hostProcess.Open();
+
                 Console.WriteLine("Lucky6 Remote servers are Running...");
+            }
+            catch (Exception e)
+            {
+                NetTcpBinding binding = new NetTcpBinding();
+                binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+                string address = "net.tcp://localhost:9902/Server";
+                hostServis = new ServiceHost(typeof(Server));
+                hostServis.AddServiceEndpoint(typeof(IServer), binding, address);
+
+                NetTcpBinding binding2 = new NetTcpBinding();
+                string address2 = "net.tcp://localhost:9912/Process";
+                hostProcess = new ServiceHost(typeof(ProcessState));
+                hostProcess.AddServiceEndpoint(typeof(IServerState), binding2, address2);
+
+                hostServis.Credentials.ClientCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.ChainTrust;
+                hostServis.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+                hostServis.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
 
 
-                Console.Read();
+                Console.WriteLine("Lucky6 Remote servers are Running...");
+            }
 
-                hostServis.Close();
-                hostProcess.Close();
+
+
+            hostProcess.Open();
+            hostServis.Open();
+
+            Console.Read();
+
+            hostServis.Close();
+            hostProcess.Close();
             }
         }
     }
